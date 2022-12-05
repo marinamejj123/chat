17c6ec83978965cf5681a55b4b3c2fe79cb92150
marinamejj123 <marinamejj123@gmail.com>
2022-12-05 15:40:42 -0400

    Updates

---
+++ precache-manifest.5f11a88c91031243addfb58da8c9b5c4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58f9fe4d2010fc1c54e207bec0416f65",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "cbefaa0ed1a5ce9ba5d4",
+    "url": "/chat/static/css/2.8e2610c1.chunk.css"
+  },
+  {
+    "revision": "9b19fde2486523af4110",
+    "url": "/chat/static/css/main.b3efca0b.chunk.css"
+  },
+  {
+    "revision": "cbefaa0ed1a5ce9ba5d4",
+    "url": "/chat/static/js/2.d381e343.chunk.js"
+  },
+  {
+    "revision": "04ccf57cace4e7ac842705b412ebf9df",
+    "url": "/chat/static/js/2.d381e343.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a238920b9cfb459191b",
+    "url": "/chat/static/js/3.2bfc4296.chunk.js"
+  },
+  {
+    "revision": "9b19fde2486523af4110",
+    "url": "/chat/static/js/main.be467f9b.chunk.js"
+  },
+  {
+    "revision": "5d2570bca4ca0bdefba3",
+    "url": "/chat/static/js/runtime-main.c98eaf0f.js"
+  }
+]);
